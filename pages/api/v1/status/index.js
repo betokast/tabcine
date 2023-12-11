858a298c63c939c1376426d0c063941e4cefe438
robertoCastro <bkastro@gmail.com>
2023-12-10 21:20:20 -0300

    Inclusão dos arquivos `compose.yaml` e `database.js` para conexão com o Postgres

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,9 @@
-function status(request, response) {
-  response
-    .status(200)
-    .json({ chave: "Teste da página de status efetuado com sucesso" });
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1");
+  console.log(result);
+  response.status(200).json({ chave: "Esse curso é acima da média" });
 }
+
 export default status;
